81a0c2ce0f28d3878a74a64bd6bc3a5bd9e9ba1a
maxime-dcps <maximedcps@hotmail.com>
2025-10-01 21:18:19 +0200

    move_player + refactor code base

--- main.c
+++ main.c
@@ -8,55 +8,113 @@
 #define ROW 6
 #define COLUMN 8
 
-void init_board(char gameBoard [ROW][COLUMN]);
-int move_player(char gameBoard[ROW][COLUMN], char player, int destRow, int destColumn);
+//STRUCTURES
+typedef struct {
+    int row;
+    int col;
+    char symbol;
+} Player;
+
+typedef struct {
+    char board [ROW][COLUMN];
+    Player p1;
+    Player p2;
+} Game;
+
+//FUNCTIONS
+void init_board(Game *game);
+void move_player(char board[ROW][COLUMN], Player * player, int destRow, int destColumn);
+int is_in_reach(Player player, int row, int col);
+int is_in_board(int row, int col);
+void set_player_pos(Player *player, int row, int column);
 void get_player_pos(char gameBoard[ROW][COLUMN], char player, int *row, int *column);
 void display_board(char gameBoard [ROW][COLUMN]);
 
 int main(void) {
 
-    char gameBoard [ROW][COLUMN];
+    Game game;
 
-    init_board(gameBoard);
+    game.p1.symbol = 'A';
+    game.p2.symbol = 'B';
+
+    init_board(&game);
 
     #ifdef DEBUG
-        int a, b;
-        get_player_pos(gameBoard, 'B', &a, &b );
-        display_board(gameBoard);
+        display_board(game.board);
+        move_player(game.board, &game.p1, 2, 1);
+        display_board(game.board);
+        move_player(game.board, &game.p1, 1, 2);
+        display_board(game.board);
+        move_player(game.board, &game.p1, 1, 4);
+        display_board(game.board);
     #endif
-
-    //disp(0);
 
     return 0;
 }
 
-void init_board(char gameBoard [ROW][COLUMN]) {
+void init_board(Game *game) {
 
     //Resetting the board
     int i,j;
     for(i = 0; i < ROW; i++) {
         for(j = 0; j < COLUMN; j++) {
-            gameBoard[i][j] = '0';
+            game->board[i][j] = '0';
         }
     }
 
-    //Placing player A and B
-    gameBoard[2][0] = 'A';
-    gameBoard[3][7] = 'B';
+    //Placing player A
+    game->board[2][0] = game->p1.symbol;
+    set_player_pos(&(game->p1), 2, 0);
+
+    //Placing player B
+    game->board[3][7] = game->p2.symbol;
+    set_player_pos(&(game->p2), 3, 7);
+
+    /* TO REFACTOR
+        split in init_board() + init_players() = init_game()
+        place_player(); //Update board and player position
+    */
 }
 
-int move_player(char gameBoard[ROW][COLUMN], char player, int destRow, int destColumn)
+void move_player(char board[ROW][COLUMN], Player * player, int destRow, int destColumn)
 {
-    int oldRow, oldColumn;
-
-    if (gameBoard[destRow][destColumn] == '0')//->REFACTOR isAvailable
+    if (is_in_board(destRow, destColumn) && is_in_reach(*player, destRow, destColumn) && board[destRow][destColumn] == '0')//->REFACTOR must be player turn
     {
-        get_player_pos(gameBoard, player, &oldRow, &oldColumn );
-        gameBoard[destRow][destColumn] = player;
+        board[player->row][player->col] = '0';
+        board[destRow][destColumn] = player->symbol;
+        set_player_pos(player, destRow, destColumn);
 
-        return 0;
+        #ifdef DEBUG
+        printf("Player %c has moved (%d, %d)\n", player->symbol, destRow, destColumn);
+        #endif
     }
-    else return -1;
+    #ifdef DEBUG
+    else {
+        if (!is_in_board(destRow, destColumn)) printf("Destination is not in board \n");
+        else if (!is_in_reach(*player, destRow, destColumn)) printf("Destination is not in reach\n");
+        else if (board[destRow][destColumn] != '0') printf("Destination is not isn't available\n");
+    }
+    #endif
+
+}
+
+//Can player move to this position
+int is_in_reach(Player player, int row, int col)
+{
+    int dx = player.row - row;
+    int dy = player.col - col;
+    return (dx*dx + dy*dy) < 4;
+}
+
+int is_in_board(int row, int col)
+{
+    return (row >= 0 && row < ROW && col >= 0 && col < COLUMN);
+}
+
+void set_player_pos(Player *player, int row, int column) {
+    //REFACTOR : verify inner condition
+    player->row = row;
+    player->col = column;
 }
 
 void get_player_pos(char gameBoard[ROW][COLUMN], char player, int *row, int *column)
