2c8d25042219b643adc3dabf706f91711b15eb6d
maxime-dcps <maximedcps@hotmail.com>
2025-10-02 12:07:44 +0200

    delete tile and get game result functions

--- main.c
+++ main.c
@@ -19,6 +19,7 @@ typedef struct {
     char board [ROW][COLUMN];
     Player p1;
     Player p2;
+    int current;
 } Game;
 
 //FUNCTIONS
@@ -27,22 +28,43 @@ void move_player(char board[ROW][COLUMN], Player * player, int destRow, int dest
 int is_in_reach(Player player, int row, int col);
 int is_in_board(int row, int col);
 void set_player_pos(Player *player, int row, int column);
-void get_player_pos(char gameBoard[ROW][COLUMN], char player, int *row, int *column);
-void display_board(char gameBoard [ROW][COLUMN]);
+void get_player_pos(char board[ROW][COLUMN], char player, int *row, int *column);
+void display_board(char board [ROW][COLUMN]);
+void delete_tile(char board[ROW][COLUMN], int row, int column);
+int get_game_result(Game game);
+int can_move(char board[ROW][COLUMN], Player player);
 
 int main(void) {
+
+    int gameResult = 0;
+    /*
+     * gameResult status codes:
+     *   0 = Game is still in progress
+     *   1 = Game over → Player 1 has won
+     *   2 = Game over → Player 2 has won
+     */
 
     Game game;
 
     game.p1.symbol = 'A';
     game.p2.symbol = 'B';
+    game.current = 1;
 
     init_board(&game);
+
+    // while (gameResult == 0 || sudden_disconnection)
+    // {
+    //     move();
+    //     delete();
+    //     game.current = game.current == 1 ? 1 : 2;
+    //     gameResult = get_game_result(game);
+    // }
 
     #ifdef DEBUG
         display_board(game.board);
         move_player(game.board, &game.p1, 2, 1);
         display_board(game.board);
+        delete_tile(game.board, 1, 2);
         move_player(game.board, &game.p1, 1, 2);
         display_board(game.board);
         move_player(game.board, &game.p1, 1, 4);
@@ -117,7 +139,7 @@ void set_player_pos(Player *player, int row, int column) {
     player->col = column;
 }
 
-void get_player_pos(char gameBoard[ROW][COLUMN], char player, int *row, int *column)
+void get_player_pos(char board[ROW][COLUMN], char player, int *row, int *column)
 {
     int i = 0, found = 0, j = 0;
 
@@ -129,7 +151,7 @@ void get_player_pos(char gameBoard[ROW][COLUMN], char player, int *row, int *col
         j = 0;
 
         while (!found && j < COLUMN) {
-            (gameBoard[i][j] == player) ? found = 1 : j++;
+            (board[i][j] == player) ? found = 1 : j++;
         }
         if (!found) i++;
     }
@@ -145,7 +167,7 @@ void get_player_pos(char gameBoard[ROW][COLUMN], char player, int *row, int *col
     #endif
 }
 
-void display_board(char gameBoard [ROW][COLUMN])
+void display_board(char board [ROW][COLUMN])
 {
     int i, j;
 
@@ -153,9 +175,46 @@ void display_board(char gameBoard [ROW][COLUMN])
 
     for (i = 0; i < ROW; i++)
     {
-        for (j = 0; j < COLUMN; j++) printf(" %c ", gameBoard[i][j]);
+        for (j = 0; j < COLUMN; j++) printf(" %c ", board[i][j]);
         printf("\n");
     }
 
     printf("------------------------\n");
+}
+
+void delete_tile(char board[ROW][COLUMN], int row, int column) {
+    if (board[row][column] == '0') board[row][column] = 'X';
+}
+
+int get_game_result(Game game)
+{
+    //Current player is either player 1 or player 2
+    Player currentPlayer = game.current == 1 ? game.p1 : game.p2;
+    Player opponent = game.current == 1 ? game.p2 : game.p1;
+    //The winner order will be his order number
+    int opponentOrder = game.current == 1 ? 2 : 1;
+
+    if (!can_move(game.board, currentPlayer)) return opponentOrder;
+
+    if (!can_move(game.board, opponent)) return game.current;
+
+    return 0; //Game is not over yet
+}
+
+int can_move(char board[ROW][COLUMN], Player player) {
+    //Arrays representing the 8 possible movement directions
+    const int rowOffset[] = {-1, -1, -1,  0,  0,  1,  1,  1};
+    const int colOffset[] = {-1,  0,  1, -1,  1, -1,  0,  1};
+
+    for (int i = 0; i < 8; i++) {
+        int tileRow = player.row + rowOffset[i];
+        int tileCol = player.col + colOffset[i];
+
+        //!\ Function order here is crucial
+        if (is_in_board(tileRow, tileCol) && board[tileRow][tileCol] == '0') {
+            return 1; // Valid move found, exit immediately
+        }
+    }
+
+    return 0; // No valid moves found after checking all 8 tiles
 }
